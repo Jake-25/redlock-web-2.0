bf0af937bb7305bf579e1a703c25edfc34423c75
Jake-25 <96160692+Jake-25@users.noreply.github.com>
2023-05-02 16:46:57 +0700

    Update redlock-db.sql

--- redlock-db.sql
+++ redlock-db.sql
@@ -1,12 +1,14 @@
 
 CREATE TABLE IF NOT EXISTS users (
-    ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    Nama VARCHAR(30) NOT NULL,
-    Alamat VARCHAR(50) NOT NULL,
-    Jabatan VARCHAR(30) NOT NULL
+    ID INT(7) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Nama VARCHAR(70) NOT NULL,
+    Alamat VARCHAR(70) NOT NULL,
+    Jabatan VARCHAR(70) NOT NULL
 );
 
 INSERT INTO users (Nama, Alamat, Jabatan) VALUES
-    ('John Doe', 'Jl. Sudirman 123', 'Manager'),
-    ('Jane Smith', 'Jl. Thamrin 456', 'Sales'),
-    ('Bob Johnson', 'Jl. Gatot Subroto 789', 'Accountant');
+    ('Ashe Blue ', 'Park Avenue', 'Employee'),
+    ('Smora Gwen', 'Redstar Avenue', 'Manager'),
+    ('yone Dan', 'Fallout Avenue', 'Engineer'),
+    ('Tim Hoogan', 'Roadstar Avenue', 'Driver'),
+    ('Clara Sam', 'Starship avenue', 'Employee');
